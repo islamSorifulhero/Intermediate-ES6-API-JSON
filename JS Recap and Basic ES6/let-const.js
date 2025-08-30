2730e172c435d98c9e93369138ead053ead9b351
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-30 12:30:29 +0600

    JS Recap and Basic ES6 Practice

---
+++ JS Recap and Basic ES6/let-const.js
@@ -0,0 +1,24 @@
+// console.log(name);
+// var name = 'vasani';
+
+
+// console.log(roll);
+// const roll = 50;
+
+
+
+// const dress = ['shirt', 'pant', 'jacket'];
+// dress.push('maflar')
+// dress.pop()
+// dress.unshift('tupi')
+// dress.shift()
+// console.log(dress);
+
+
+const student = {
+    name: 'jaglul',
+    marks: 50
+}
+// student.marks = 80;
+student.name = 'md. jaglul islam'
+console.log(student);
