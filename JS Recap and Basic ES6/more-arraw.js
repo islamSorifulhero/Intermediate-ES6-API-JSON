997318a71d6ad389e504f2ee6536d2e4546c6798
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-30 15:31:14 +0600

    More arraw practice done

---
+++ JS Recap and Basic ES6/more-arraw.js
@@ -0,0 +1,25 @@
+// 20% discount
+// 20/100*100
+const getTax = (amount, taxRate) => amount * taxRate / 100;
+const add = (p, q) => p + q;
+
+//single parameter
+const getSquare = (x) => x * x;
+console.log(getSquare(5));
+
+const getHalf = num => num / 2;
+console.log(getHalf(18));
+
+const firstElement = num => num[0];
+console.log(firstElement([25, 33, 75]));
+
+const logIt = () => console.log(78);
+console.log(logIt());
+
+// document.getElementById('').addEventListener('click', event =>{
+
+// })
+
+// () => {
+
+// }
