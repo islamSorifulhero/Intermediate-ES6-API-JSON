9191821477669130349372b77cd311312d9f1a15
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-30 19:40:44 +0600

    Keys Values entries prictice done

---
+++ JS Recap and Basic ES6/entries.js
@@ -0,0 +1,16 @@
+const employee = {
+    name: 'Raja Rani',
+    designation: 'QA',
+    salary: '20000',
+    experience: 1
+}
+
+const key = Object.keys(employee)
+// console.log(key);
+
+const value =Object.values(employee)
+// console.log(value);
+
+const entry = Object.entries(employee)
+console.log(entry);
+
