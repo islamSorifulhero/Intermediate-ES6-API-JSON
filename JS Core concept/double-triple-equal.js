d5f21aad5a9be2b07ccd65ccf91f140ad35e7550
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-09-01 16:50:09 +0600

    double equal and triple equal practice done

---
+++ JS Core concept/double-triple-equal.js
@@ -0,0 +1,37 @@
+// console.log(2 == 2);
+// console.log(2 == '2');
+// console.log(2 == 1);
+// console.log(true == 1);
+// console.log(false == 1);
+// console.log(false == 0);
+// console.log(true == '1');
+// console.log(false == '0');
+// console.log(null == undefined);
+// console.log(NaN == NaN);
+// console.log([] == '');
+// console.log([5] == '5');
+// console.log([5] == 5);
+// console.log({} == {})
+// console.log([5] == [5])
+// const p = [4, 5, 7, 8]
+// const q = p;
+// console.log(p == q)
+// console.log([5].toString())
+
+
+
+
+// === first check the type. if type is different, they are different
+
+//if type is same then checks the value, if value is same--> true, if value is false --> false
+
+
+//under the all answer false
+
+// console.log(2 === '2');
+// console.log(1 === 2);
+// console.log(true === 1);
+// console.log(false === 0);
+// console.log(null === undefined);
+// console.log(0 === '');
+// console.log([] === '');
