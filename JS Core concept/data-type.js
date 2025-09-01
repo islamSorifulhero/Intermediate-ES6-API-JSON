ecf55ac5b3424f8a61fb4ba6e607090b80230da2
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-09-01 15:36:51 +0600

    data type practice done

---
+++ JS Core concept/data-type.js
@@ -0,0 +1,19 @@
+let data = 4;
+data = 'tormuj';
+console.log(typeof data);
+
+//js: primitive data type
+const price  = 50;
+const name = 'salman';
+const isStudent = true;
+let isKnown = undefined;
+let merit = null;
+console.log(isKnown);
+
+
+// non primitive
+const names = ['tom', 'bob', 'jack']
+const student = {
+    name: 'salam',
+    age: 22
+}
