3210f6a658991146a336021cdd3783e8501682b2
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-09-01 15:50:38 +0600

    undefined null practice done

---
+++ JS Core concept/undefined.js
@@ -0,0 +1,10 @@
+let money;
+// console.log(money);
+
+function total(a,b){
+    console.log('value of parameters', a, b);
+}
+// total(2)
+
+const friends = ['salam', 'kalam', 'jamal']
+console.log(friends[3]);
