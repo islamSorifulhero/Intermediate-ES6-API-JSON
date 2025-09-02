7b9ca3c9456bf68f1d048f6ccc6124b0186817c9
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-09-02 12:03:58 +0600

    template string practice done

---
+++ ES6-some-core-concept/template-string.js
@@ -0,0 +1,11 @@
+var age = 15;
+var name =  `Ahashan Habib
+utsho. My age is ${age}. I am ${age < 18? 'not' : ''} adult.`;
+console.log(name);
+
+
+// padStart, padEnd, repeat
+var test = 'demo';
+console.log(test.padStart(10, '*'));
+console.log(test.padEnd (15, '-'));
+console.log(test.repeat(20));
