30d19e8914e9c6cb13c22b04cfd8d3fb13a17948
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-09-02 20:37:17 +0600

    primitive non-primitive truthy falsy

---
+++ ES6-some-core-concept/primitive-nonprimitive-truthy-falsy.js
@@ -0,0 +1,35 @@
+// 2 types data type
+// 1. Primitive = string, number, boolean, undefined, null, Symbol, BigInt
+// 2. Non primitive or referential= Object, Array, Function, Date
+
+// Primitive example
+let age = 18;
+let age2 = age;
+age2 = 30;
+// console.log("age2-> ", age2, "age-> ", age);
+
+
+
+
+// Non primitive or Referential
+const obj = {
+  a: 10,
+  b: 20,
+};
+//const obj2 = obj; //assigned reference or location
+const obj2 = { ...obj }; // create independent obj using spread operator
+obj2.a = 50;
+
+// console.log("obj2-> ", obj2);
+// console.log("obj-> ", obj);
+
+
+//truthy falsy
+// Falsy value = false, 0, "", null, undefined, NaN
+//Truthy value = falsy value bad diye baki shob holo truthy value
+//0 er jaigai value gulo change kore dekho konta ki???
+if (0) {
+  console.log("Ami truthy value");
+} else {
+  console.log("Ami falsy value");
+}
