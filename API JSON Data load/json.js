ffdb83f8322d10657ca55f9057391752b5a850db
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-09-03 10:01:22 +0600

    JSON releted practice

---
+++ API JSON Data load/json.js
@@ -0,0 +1,21 @@
+console.log('Explore API');
+
+const person = {
+    name: 'selim',
+    fruit: 'dalim',
+    dish: 'halim',
+    friends: ['alim', 'kolim', 'lamim'],
+    isRich: false,
+    money: 34000,
+};
+console.log(person, typeof person);
+// json -> javaScript object Notation
+// JSON.stringify -> JSON
+// JSON.parse -> Object
+// const personJSON = JSON.stringify(person);
+// console.log(personJSON, typeof personJSON);
+
+// const parseJSON = JSON.parse(personJSON);
+// // console.log(parseJSON);
+
+
