e8078c408c1af548ffef7a7bbfc2bb5def85df91
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-09-14 23:46:44 +0600

    await practice done

---
+++ Bonus Local Storage/await.js
@@ -0,0 +1,14 @@
+function getApple (){
+    setTimeout (() => {
+        console.log('This is Apple');
+    }, 3000);
+}
+
+function getBanana(){
+    setTimeout(() =>{
+        console.log('Banana good');
+    }, 1000)
+}
+
+getApple()
+getBanana()
