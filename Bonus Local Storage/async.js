cb67c66e5f6e9ec4df28923ac350c8959b60db03
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-09-14 15:39:50 +0600

    async await practice done

---
+++ Bonus Local Storage/async.js
@@ -0,0 +1,55 @@
+// console.log('one');
+// console.log('two');
+// console.log('three');
+// console.log('four');
+// console.log('five');
+
+
+// console.log('one');
+// console.log('two');
+// three()
+// console.log('four');
+// console.log('five');
+
+// function three() {
+//     console.log('three');
+// }
+
+
+// console.log('one');
+// console.log('two');
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((res) => res.json())
+//     .then((json) => console.log(json))
+
+// console.log('four');
+// console.log('five');
+
+
+// console.log('one');
+// console.log('two');
+// // three()
+// setTimeout (three, 5000)
+
+// console.log('four');
+// console.log('five');
+
+// function three() {
+//     console.log('three');
+// }
+
+console.log('one');
+console.log('two');
+
+// setTimeout (three, 5000)
+setTimeout(() => {
+    console.log('new three inside set time out');
+}, 3000);
+
+console.log('four');
+console.log('five');
+
+function three() {
+    console.log('three');
+}
